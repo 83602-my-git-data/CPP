49078d020175e687a9ac49fa22c7ebd8f46ea6e1
shamgirhe <shamgirhe01@gmail.com>
2024-03-20 15:09:59 +0530

    Assignment 2

--- Assignment2/Q1.cpp
+++ Assignment2/Q1.cpp
@@ -1,5 +1,10 @@
 /* Write a menu driven program to calculate volume of the box(length * width * height).
-Provide necessary constructors*/
+Provide parameterless, parameterized(with 3 parameters) and single paramaterized constructor.
+Create the local objects in respective case and call the function to caluclate area.
+Menu options -> 
+1. Calculate Volume with default values
+2. Calculate Volume with length,breadth and height with same value
+3. Calculate Volume with different length,breadth and height values*/
 
 #include <iostream>
 using namespace std; 
@@ -9,16 +14,20 @@ class Box{
      float length, width, height;
 
     public:
-     Box(){
-         length = 0;
-         width = 0;
-         height = 0;
+     Box():Box(0,0,0){
+        cout<<" parameterless constructor"<<endl;
      }
+
+     Box(float length):Box(length,length,length){
+            cout<<"1 parameterized constructor"<<endl;
+     };
+
       Box(float length, float width, float height){
+        cout<<"3 parameterized constructor"<<endl;
         this->length = length;
         this->width = width;
         this->height = height;
-      } 
+      }   
      float volume(){
         return length * width * height;
      } 
@@ -27,11 +36,12 @@ class Box{
 int main(){
     int choice = 0;
     float length, width, height;
-    Box *box = new Box();
+   // Box *box = new Box();
     do{
          cout<<"0.Exits"<<endl;
-         cout<<"1.Enter Length , Width and Height"<<endl;
-         cout<<"2.Calculate Volume "<<endl;
+         cout<<"1. 3 parameterized constructor"<<endl;
+         cout<<"2. 1 parameterized constructor"<<endl;
+         cout<<"3.0 parameterized constructor"<<endl;
 
          cout<<"Enter your choice: ";
          cin>>choice;
@@ -39,7 +49,7 @@ int main(){
             switch(choice){
             case 0:
                 break;
-            case 1:
+            case 1:{
                 cout<<endl;
                 cout<<"Enter Length: ";
                 cin>>length;
@@ -48,11 +58,22 @@ int main(){
                 cout<<"Enter Height: ";
                 cin>>height;
 
-                box = new Box(length,width,height);
+             Box box = Box(length,width,height);
+               cout<<"Volume ::- "<<box.volume()<<endl;
+               
+            };
+            break;
+            case 2:{
+                    cout<<"Enter Length :: "<<endl;
+                    cin>>length;
+                    cout<<"Volume :: "<<Box(length).volume()<<endl;
+                };
                 break;
-            case 2:
-                cout<<"Volume: "<<box->volume()<<endl;
-                break;
+
+             case 3:{
+                    cout<<"Volume :: "<<Box().volume()<<endl;
+                };
+                break;    
             default:
                 cout<<"Invalid choice"<<endl;
                 break;
